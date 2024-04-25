a9e0631136d2e5617927ae3de81e57c68273e08c
shishiraps <psshishira56@gmail.com>
2024-04-25 11:17:48 +0530

    second commit

--- Helloworld/src/Samplepackage/Sample.java
+++ Helloworld/src/Samplepackage/Sample.java
@@ -7,6 +7,7 @@ public class Sample {
 		System.out.print("Hi, hello, how are u?");
 		System.out.print("Hey whre are you");
 		System.out.print("you are successful");
+		System.out.println("my name");
 
 	}
 
